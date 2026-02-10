b740f820e929e928bce68117b6fe8fa233020c3d
Anan <an@anatdx.com>
2026-02-10 20:14:54 +0800

    refactor: update manager signatures and hashes
    - Renamed first and second manager identifiers for clarity to YukiSU Manager and Rei Manager.
    - Swapped the expected hash values for the first and second managers to ensure correct integrity verification.
    - Updated murasaki-api subproject commit to reflect the latest changes.

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -1,15 +1,15 @@
 #ifndef MANAGER_SIGN_H
 #define MANAGER_SIGN_H
 
-// First Manager
+// YukiSU Manager
 #define EXPECTED_SIZE_FIRST 0x29c
 #define EXPECTED_HASH_FIRST                                                    \
-	"6eafa78ef61acedcb19facd0387e42046a6614126782620244def709f9a84c7e"
+	"39559b380d4c0191eed81b7eba61533b6a2f247bc55bceba4259e983673f58b7"
 
-// Second Manager
+// Rei Manager
 #define EXPECTED_SIZE_SECOND 0x29c
 #define EXPECTED_HASH_SECOND                                                   \
-	"39559b380d4c0191eed81b7eba61533b6a2f247bc55bceba4259e983673f58b7"
+	"6eafa78ef61acedcb19facd0387e42046a6614126782620244def709f9a84c7e"
 
 typedef struct {
 	unsigned size;
