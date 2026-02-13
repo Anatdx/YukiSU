b4c9f9b86b5faa44b905be1ffcae4f635e9a6d78
Anan <an@anatdx.com>
2026-02-13 09:40:40 +0800

    chore: update version code and name in defs.cpp; update ndk-busybox submodule
    - Incremented version code to 13209 and version name to 1.3.0-24-g2575d635 in defs.cpp.
    - Updated ndk-busybox submodule to the latest commit 75e588f1552c698f6cd02a00b74e1a9ae53be0e9.

--- userspace/ksud/src/defs.cpp
+++ userspace/ksud/src/defs.cpp
@@ -3,7 +3,7 @@
 namespace ksud {
 
 // Auto-generated at build time
-const char* VERSION_CODE = "13206";
-const char* VERSION_NAME = "1.3.0-21-g2d7ad5c5";
+const char* VERSION_CODE = "13209";
+const char* VERSION_NAME = "1.3.0-24-g2575d635";
 
 }  // namespace ksud
